9e7d45edaa4916200734efb314d5b5705d14be60
Dante-Capuzzi <dec00010@mix.wvu.edu>
2024-02-29 17:41:49 -0500

    #5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './Card';
+import NavBar from './Navbar';
 
 function App() 
 {
   return (
     <div className="App">
+      <NavBar />
       <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800364829</p>
       <p>Hi I am Dante Capuzzi</p>
-      <p>4</p>
-      <Card />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Card()
 {
   return (
-    <div className="card" style={{ width: '18rem' }}>
+    <div className="card" style={{ width: '18rem', marginRight: '10px' }}>
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
