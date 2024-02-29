c600c46b1ab08400ee842bbeb660b2a146ad4dbd
Dante-Capuzzi <dec00010@mix.wvu.edu>
2024-02-29 17:22:28 -0500

    4th try

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import './Navbar.js';
-import Card from './Card.js';
+import Card from './Card';
 
 function App() 
 {
   return (
     <div className="App">
-      <nav></nav>
       <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800364829</p>
       <p>Hi I am Dante Capuzzi</p>
-      <p>3</p>
+      <p>4</p>
       <Card />
     </div>
   );
