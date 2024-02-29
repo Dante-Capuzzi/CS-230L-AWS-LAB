aa2664eb8121556cd3ea721db438ef13c4ddcc80
Dante-Capuzzi <dec00010@mix.wvu.edu>
2024-02-29 18:28:32 -0500

    Lab 8 try #7

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,7 +14,7 @@ function NavBar()
       </div>
 
       <form className="form-inline" style={{ display: 'flex', justifyContent: 'flex-end', marginRight: '10px' }}>
-        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style={{marginRight: '10px'}}></input>
         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
 
